cbe11147c9b6468c8a803a3810c8c6a9374601ce
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-03-05 01:00:23 +0530

    Add files via upload

---
+++ convertarrray2Llist.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr) {
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+void printLL(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    vector<int> arr = {12, 5, 8, 7};
+
+    Node* head = convertArr2LL(arr);
+
+    cout << "Linked List: ";
+    printLL(head);
+
+    return 0;
+}
