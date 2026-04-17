11388d65e72e981f3e888840201c426d1c22c173
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-04-17 09:36:26 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,8 @@
+def check_voting_eligibility(age): 
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+
+# Function call
+check_voting_eligibility(12)
